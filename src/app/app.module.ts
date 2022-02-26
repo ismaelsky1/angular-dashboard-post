39659dc998df49d0b383fd3d503e2bfd9999b5c5
ismaelsky1 <ismael.sky1@hotmail.com>
2022-02-26 20:58:45 -0300

    init chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCommonModule } from '@angular/material/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { PostComponent } from './screen/post/post.component';
 import { DashboardComponent } from './screen/dashboard/dashboard.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent, PostComponent],
@@ -17,9 +30,24 @@ import { DashboardComponent } from './screen/dashboard/dashboard.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgApexchartsModule,
+
+    MatCommonModule,
+    MatButtonModule,
 
     MatSidenavModule,
+    MatToolbarModule,
     MatListModule,
+    MatGridListModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/screen/dashboard/dashboard.component.ts
+++ src/app/screen/dashboard/dashboard.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartComponent } from "ng-apexcharts";
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart
+} from "ng-apexcharts";
 
+export type ChartOptions = {
+  series?: ApexNonAxisChartSeries;
+  chart?: ApexChart;
+  responsive?: ApexResponsive[];
+  labels?: any;
+};
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.sass']
+  styleUrls: ['./dashboard.component.sass'],
 })
 export class DashboardComponent implements OnInit {
+   @ViewChild("chart") chart: ChartComponent | any;
+  public chartOptions: Partial<ChartOptions> | any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.chartOptions = {
+      series: [44, 55],
+      chart: {
+        width: 380,
+        type: "pie"
+      },
+      labels: ["Team A", "Team B"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
   }
 
+  ngOnInit(): void {}
 }
